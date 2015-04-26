05a982fec9b719296e408270d9f0a5bcbb861d93
radical-edo <radical.3do@gmail.com>
2015-04-26 18:31:34 +0200

    bootstrapping all the factoies
    its gonna be more convenient this way. don’t feel like requiring files
    manually.

--- tests/mocha_helper.js
+++ tests/mocha_helper.js
@@ -1,7 +1,11 @@
 require('coffee-script/register');
 var path = require('path');
+var strap = require('node-strap');
 
 global.expect = require('chai').expect;
 global.context = describe;
 global.__rootdir = path.normalize(__dirname + '/..');
 global.__testdir = __dirname;
+global.Factory = require('rosie').Factory
+
+strap.files(__testdir + '/factories');
